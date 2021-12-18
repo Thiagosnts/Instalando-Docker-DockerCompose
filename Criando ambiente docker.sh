9a666f239299db786e38017a2e752079b661cc54
Thiagosnts <39925677+Thiagosnts@users.noreply.github.com>
2021-12-18 10:03:04 -0300

    ajuste Readme

--- Criando ambiente docker.sh
+++ Criando ambiente docker.sh
@@ -1,16 +1,4 @@
-#Ambiente Docker
-sudo apt update && sudo apt upgrade -y &&
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y &&
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &&
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" &&
-sudo apt update &&
-apt-cache policy docker-ce &&
-sudo apt install docker-ce -y &&
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
-sudo chmod +x /usr/local/bin/docker-compose &&
-sudo usermod -aG docker azureuser &&
-sudo reboot
-#Ambiente Docker
+
 
 #buildando imagem
 git clone https://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@github.com/Thiagosnts/Jogo-da-Velha.git &&
